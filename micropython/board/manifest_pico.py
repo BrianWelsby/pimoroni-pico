4d1c92dafa0c0df16de50d3434c48c13c87878ce
BrianWelsby <brian@welsby.org>
2023-08-22 17:09:41 +0100

    Update manifest_pico.py

--- micropython/board/manifest_pico.py
+++ micropython/board/manifest_pico.py
@@ -2,4 +2,4 @@ freeze("../modules_py", "gfx_pack.py")
 freeze("../modules_py", "interstate75.py")
 
 freeze("../modules_py", "pimoroni.py")
-freeze("../modules_py", "boot.py")
+# freeze("../modules_py", "boot.py")
